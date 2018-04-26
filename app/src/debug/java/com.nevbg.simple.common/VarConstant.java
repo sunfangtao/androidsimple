b108d50626b24367c1e8007994a7062b36ae6b9d
sunfangtao <sunfangtao@aioute.com>
2018-04-26 16:29:48 +0800

    增加测试和生产环境的配置，2个接口的url添加

---
+++ app/src/debug/java/com.nevbg.simple.common/VarConstant.java
@@ -0,0 +1,10 @@
+package com.nevbg.simple.common;
+
+/**
+ * Created by Administrator on 2018/4/26.
+ */
+
+public class VarConstant {
+    // TODO 修改测试服务器的信息
+    public static final String IP_PORT = "http://221.0.91.34:3080/apptransponder";
+}
---
+++ app/src/main/java/com/nevbg/simple/common/HttpConstant.java
@@ -0,0 +1,15 @@
+package com.nevbg.simple.common;
+
+/**
+ * Created by Administrator on 2018/4/26.
+ */
+
+public class HttpConstant {
+
+    public static String IP_PORT = VarConstant.IP_PORT;
+
+    public static final class HttpUrl {
+        public static final String GENERAL_URL = IP_PORT + "/trans/app";
+        public static final String PIC_URL = IP_PORT + "/trans/pic";
+    }
+}
